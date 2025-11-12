116e1871a4f0f2564e6b64c77bde4e672abb706c
abaragithan <abaragithan02@gmail.com>
2025-11-12 21:51:14 +0530

    Fix: Vercel deployment issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-      }
-    }
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist'
   }
-});
-
+})
